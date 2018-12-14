9b6412df8995c9e834d38130df7e23118c28bda7
tristan-laroubine <laroubine.tristan@etu.univ-amu.fr>
2018-12-14 20:57:29 +0100

    coucou

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ widthScreenSize = c.clientWidth;
 heightScreenSize = c.clientHeight;
 var ctx = c.getContext("2d");
 
-var playerImg = new Image();   // Crée un nouvel élément Image
-playerImg.src = "https://via.placeholder.com/100x100";
 
 
 class Mobile{
@@ -34,8 +32,27 @@ class Mobile{
 function start() {
     window.requestAnimationFrame(draw);
 }
+deplacementImage = 0;
+frameDepalcement = 0;
+function drawImageSprit(Image,row, cols, posX, posY, widthCanvas, HeightCavans){
+    ctx.drawImage(Image,row*64,cols*64,64,64,0,0,200,200);
+}
 function draw() {
-    ctx.fillRect(0,0,50,50);
-    ctx.drawImage(playerImg,50,50);
+    ctx.fillRect(0,0,500,500);
+
+    var playerImg = new Image();   // Crée un nouvel élément Image
+    playerImg.src = "img/skinPlayer/1.png";
+    drawImageSprit(playerImg,8,0,64,64);
+    if (frameDepalcement%5 === 0){
+        deplacementImage = deplacementImage + 1;
+    }
+
+    ++frameDepalcement;
+    if (deplacementImage >= 64*9)
+    {
+        deplacementImage = 0;
+    }
+    window.requestAnimationFrame(draw);
+
 }
 start();
