b49238681dc49e32549da27cb96a9a88644e43be
LAROUBINE Tristan <l17002521@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-17 15:20:51 +0100

    voila

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ class Mobile{
         this.vitesse = vitesse;
         this.mobileImg = new Image();
         this.mobileImg.src = skin;
+        this.deplacementImage = 0;
+        this.frameDeplacement = 0;
     }
     walk(){
 
@@ -24,9 +26,41 @@ class Mobile{
 
     }
     draw(){
-        ctx.drawImage(this.mobileImg,x,y);
+        ++this.frameDeplacement;
     }
+    drawImageSprit(row, posX, posY, width, height){
+        if (this.frameDeplacement %5===0)
+        {
+            ++deplacementImage;
+        }
+        if (row>= 0 && row <= 4)
+        {
+            if (deplacementImage>=7) deplacementImage=0;
 
+        }
+        else if(row >= 5 && row <= 8){
+            if (deplacementImage>=8) deplacementImage=0;
+
+        }
+        else if(row >= 9 && row <= 12){
+            if (deplacementImage>= 9) deplacementImage=0;
+        }
+        else if (row >= 13 && row <= 17){
+
+            if (deplacementImage>= 6) deplacementImage=0;
+
+        }
+        else if (row >= 18 && row <= 22){
+            if (deplacementImage>= 13) deplacementImage=0;
+        }
+        else if (row >= 23){
+            if (deplacementImage>= 6 )
+            {
+                deplacementImage=0;
+            }
+        }
+            ctx.drawImage(this.mobileImg,deplacementImage*64,row*64,64,64,posX,posY,width,height);
+    }
 }
 
 function start() {
@@ -34,24 +68,14 @@ function start() {
 }
 deplacementImage = 0;
 frameDepalcement = 0;
-function drawImageSprit(Image,row, cols, posX, posY, widthCanvas, HeightCavans){
-    ctx.drawImage(Image,row*64,cols*64,64,64,0,0,200,200);
-}
+
 function draw() {
     ctx.fillRect(0,0,500,500);
 
-    var playerImg = new Image();   // Crée un nouvel élément Image
-    playerImg.src = "img/skinPlayer/1.png";
-    drawImageSprit(playerImg,8,0,64,64);
-    if (frameDepalcement%5 === 0){
-        deplacementImage = deplacementImage + 1;
-    }
 
-    ++frameDepalcement;
-    if (deplacementImage >= 64*9)
-    {
-        deplacementImage = 0;
-    }
+    let testMobile = new Mobile("img/skinPlayer/1.png",0,0,64,64,5);
+    // testMobile.draw();
+    testMobile.drawImageSprit(0,0,0,200,200);
     window.requestAnimationFrame(draw);
 
 }
